4792ca4e27fe536bbd670581e41654248daea09e
USHA SRI PONAKA <144483101+ushasri3110@users.noreply.github.com>
2024-03-29 00:48:18 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (49.87%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int pos=0;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=0){
+                nums[pos++]=nums[i];
+            }
+        }
+        while (pos<nums.length){
+            nums[pos++]=0;
+        }
+    }
+}
