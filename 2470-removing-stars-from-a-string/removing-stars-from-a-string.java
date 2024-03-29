11c03d5219afb54d65c480afa3242e90f628ff6f
USHA SRI PONAKA <144483101+ushasri3110@users.noreply.github.com>
2024-03-29 18:59:22 +0530

    Time: 449 ms (25.07%) | Memory: 46.4 MB (41.22%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,21 +1,24 @@
+import java.util.Stack;
+
 class Solution {
     public String removeStars(String s) {
-        Stack<Character> stack=new Stack<>();
-        String str="";
-        for (int i=0;i<s.length();i++){
-            if (s.charAt(i)!='*'){
+        Stack<Character> stack = new Stack<>();
+        StringBuilder strBuilder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '*') {
                 stack.push(s.charAt(i));
-            }
-            else{
-                if (!stack.isEmpty()){
+            } else {
+                if (!stack.isEmpty()) {
                     stack.pop();
                 }
             }
         }
-        while (!stack.isEmpty()){
-            str=stack.pop()+str;
+
+        while (!stack.isEmpty()) {
+            strBuilder.insert(0, stack.pop());
         }
-        return str;
-        
+
+        return strBuilder.toString();
     }
 }
