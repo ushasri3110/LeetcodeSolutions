af104750c4387422dd33b4e185e7195499166128
USHA SRI PONAKA <144483101+ushasri3110@users.noreply.github.com>
2024-04-02 09:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.92%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,13 +1,13 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        String bigger=str1.length()>str2.length()?str1:str2;
         String smaller=str1.length()>str2.length()?str2:str1;
-        if (bigger.equals(smaller)){
+        String larger=str1.length()>str2.length()?str1:str2;
+        if (smaller.equals(larger)){
             return smaller;
         }
-        if (!bigger.startsWith(smaller)){
+        if (!larger.startsWith(smaller)){
             return "";
         }
-        return gcdOfStrings(bigger.substring(smaller.length()),smaller);
+        return gcdOfStrings(larger.substring(smaller.length()),smaller);
     }
 }
