65bc108d6adafda57c752cdfa931cb975f885727
USHA SRI PONAKA <144483101+ushasri3110@users.noreply.github.com>
2024-04-10 12:43:33 +0530

    Time: 5 ms (68.64%) | Memory: 45 MB (29.19%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m=word1.length();
+        int n=word2.length();
+        int dp[][]=new int[m+1][n+1];
+        for (int i=1;i<=m;i++){
+            dp[i][0]=i;
+        }
+        for (int j=1;j<=n;j++){
+            dp[0][j]=j;
+        }
+        for (int i=1;i<=m;i++){
+            for (int j=1;j<=n;j++){
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else{
+                    int topLeft = dp[i - 1][j - 1];
+                    int top = dp[i - 1][j];
+                    int left = dp[i][j - 1];
+                    dp[i][j] = Math.min(topLeft, Math.min(top, left)) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
