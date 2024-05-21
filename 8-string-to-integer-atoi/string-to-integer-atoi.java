15858318105c62f8202dc55166dd2c1c727b904c
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-05-21 11:12:16 +0530

    Time: 2 ms (35.44%) | Memory: 42.4 MB (31.97%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int myAtoi(String s) {
+        s=s.trim();
+        if (s.length()<=0){
+            return 0;
+        }
+        int index=0;
+        int num=0;
+        boolean isNegative=false;
+        if (s.charAt(0)=='-'||s.charAt(0)=='+'){
+            isNegative=s.charAt(0)=='-';
+            index++;
+        }
+        while (index<s.length()){
+            char c=s.charAt(index);
+            if (c<'0'||c>'9'){
+                break;
+            }
+            int digit=c-'0';
+            if (num>(Integer.MAX_VALUE-digit)/10){
+                return isNegative?Integer.MIN_VALUE:Integer.MAX_VALUE;
+            }
+            num=num*10+digit;
+            index++;
+            
+        }
+        return isNegative?num*-1:num;
+    }
+}
