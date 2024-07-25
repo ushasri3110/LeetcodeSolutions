918e236ff02a49af9f7bfac5149a2740bae241b9
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-07-25 22:58:33 +0530

    Time: 1 ms (86.26%) | Memory: 40.7 MB (43.12%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,24 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x==0 || x==1){
-            return x;
-        }
-        int left=0;
-        int right=x;
+        int low=0;
+        int high=x;
         int mid=-1;
-        while (left<=right){
-           mid=(left+right)/2;
+        while (low<=high){
+            mid=(low+high)/2;
             if ((long)mid*mid>(long)x){
-                right=mid-1;
+                high=mid-1;
             }
             else if (mid*mid==x){
+                
                 return mid;
             }
-            else
-            {
-                left=mid+1;
+            else{
+                low=mid+1;
             }
         }
-        return right;
+        return high;
     }
 }
