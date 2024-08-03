851119c05ad5e645300efe72fb34e7119a25ca7e
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-08-03 10:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (14.98%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -18,9 +18,6 @@ class Solution {
         if (root==null){
             return 0;
         }
-        int count=1;
-        count+=countNodes(root.left);
-        count+=countNodes(root.right);
-        return count;
+        return 1+countNodes(root.left)+countNodes(root.right);
     }
 }
