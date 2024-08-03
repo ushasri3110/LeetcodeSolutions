bee4fc256b190c5037bbb0950978ca81de368c16
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-08-03 10:46:55 +0530

    Time: 11 ms (71.18%) | Memory: 57.9 MB (57.98%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for (int i:nums){
+            if (map.containsKey(i)){
+                return true;
+            }
+            else{
+                map.put(i,1);
+            }
+        }
+        return false;
+    }
+}
