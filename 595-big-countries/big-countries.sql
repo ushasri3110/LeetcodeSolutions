a77755a0c62858bb7b41812aaa8e386af73e0c72
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-08-20 11:56:01 +0530

    Time: 406 ms (40.04%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,2 +1,2 @@
 /* Write your PL/SQL query statement below */
-SELECT name,population,area FROM World WHERE area>=3000000 OR population>=25000000;
+select name, population, area from world where area >= 3000000 or  population  >= 25000000;
