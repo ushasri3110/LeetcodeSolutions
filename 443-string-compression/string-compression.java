7d3ca8c69ef6d59df0bdad504c14e373ce48cb3c
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-12-27 21:57:37 +0530

    Time: 1 ms (99.66%) | Memory: 44.7 MB (8.47%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -3,16 +3,17 @@ class Solution {
         int count=1;
         int k=0;
         for (int i=0;i<chars.length;i++){
-            if (i+1<chars.length&&chars[i]==chars[i+1]){
+            if (i+1<chars.length && chars[i]==chars[i+1]){
                 count++;
             }
             else{
                 chars[k]=chars[i];
                 k++;
                 if (count>1){
-                    char[] countChars = String.valueOf(count).toCharArray();
-                    for (char c : countChars) {
-                        chars[k++] = c; 
+                    char arr[]=String.valueOf(count).toCharArray();
+                    for(char c:arr){
+                        chars[k]=c;
+                        k++;
                     }
                 }
                 count=1;
