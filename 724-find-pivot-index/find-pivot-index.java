3b9d5f126135aeef1aef0b434fc1eca9a37db2e5
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-12-28 16:46:13 +0530

    Time: 1 ms (94.65%) | Memory: 46.4 MB (5.66%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -3,16 +3,16 @@ class Solution {
         int left[]=new int[nums.length];
         int right[]=new int[nums.length];
         left[0]=0;
-        right[nums.length-1]=0;
         for (int i=1;i<nums.length;i++){
             left[i]=left[i-1]+nums[i-1];
         }
-        for (int j=nums.length-2;j>=0;j--){
-            right[j]=right[j+1]+nums[j+1];
+        right[nums.length-1]=0;
+        for (int i=nums.length-2;i>=0;i--){
+            right[i]=right[i+1]+nums[i+1];
         }
-        for (int k=0;k<nums.length;k++){
-            if (left[k]==right[k]){
-                return k;
+        for (int i=0;i<nums.length;i++){
+            if (left[i]==right[i]){
+                return i;
             }
         }
         return -1;
