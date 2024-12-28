384c2a4c12f028257a606725985c321f1cf0f06a
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-12-28 18:51:27 +0530

    Time: 9 ms (96.86%) | Memory: 45.6 MB (24.63%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,20 +1,28 @@
 class Solution {
-    public List<Integer> getElementsOnlyInFirstList(int[] nums1,int[] nums2){
-        Set<Integer> onlyInNums1=new HashSet<>();
-        Set<Integer> existsInNums2=new HashSet<>();
-        for (int i:nums2){
-            existsInNums2.add(i);
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> set1=new HashSet();
+        Set<Integer> set2=new HashSet();
+        for (int i:nums1){
+            set1.add(i);
         }
-        for (int j:nums1){
-            if (!existsInNums2.contains(j)){
-                onlyInNums1.add(j);
+        for (int i:nums2){
+            set2.add(i);
+        }
+        List<List<Integer>> answer=new ArrayList<>();
+        List<Integer> list1=new ArrayList<>();
+        List<Integer> list2=new ArrayList<>();
+        for(int i:set1){
+            if (!set2.contains(i)){
+                list1.add(i);
             }
         }
-        return new ArrayList<>(onlyInNums1);
-
-    }
-
-    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        return Arrays.asList(getElementsOnlyInFirstList(nums1,nums2),getElementsOnlyInFirstList(nums2,nums1));
+        for (int i:set2){
+            if (!set1.contains(i)){
+                list2.add(i);
+            }
+        }
+        answer.add(list1);
+        answer.add(list2);
+        return answer;
     }
 }
