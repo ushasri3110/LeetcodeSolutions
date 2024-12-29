06b5562f1b91e19cee6578a44aab246a7132debd
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-12-29 21:59:12 +0530

    Time: 5 ms (12.57%) | Memory: 42.4 MB (5.35%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,27 +1,29 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> numStack = new Stack<>();
-        Stack<String> strStack = new Stack<>();
-        String output = ""; 
-        int num = 0;
-        
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0'); 
-            } else if (ch == '[') {
+        Stack<Integer> numStack=new Stack<>();
+        Stack<String> strStack=new Stack<>();
+        int num=0;
+        String output="";
+        for(char ch:s.toCharArray()){
+            if (Character.isDigit(ch)){
+                num=num*10+(ch-'0');
+            }
+            else if (ch=='['){
                 numStack.push(num);
                 strStack.push(output);
-                output = ""; 
-                num = 0;
-            } else if (ch == ']') {
-                StringBuilder sb = new StringBuilder(strStack.pop());
-                int repeatTimes = numStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    sb.append(output);
+                num=0;
+                output="";
+            }
+            else if (ch==']'){
+                int repeatCount=numStack.pop();
+                String current=strStack.pop();
+                for (int i=0;i<repeatCount;i++){
+                    current=current+output;
                 }
-                output=sb.toString(); 
-            } else {
-                output+=ch; 
+                output=current;
+            }
+            else{
+                output+=ch;
             }
         }
         return output;
