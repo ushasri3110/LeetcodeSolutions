721772964eab71dcb3c66d4c87ea7da305ee0d30
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-12-29 19:06:03 +0530

    Time: 34 ms (51.95%) | Memory: 48.7 MB (81.51%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -3,15 +3,16 @@ class Solution {
         int count=0;
         Map<String,Integer> rowMap=new HashMap<>();
         for (int[] row:grid){
-            String rowString=Arrays.toString(row);
-            rowMap.put(rowString,rowMap.getOrDefault(rowString,0)+1);
+            String r=Arrays.toString(row);
+            rowMap.put(r,rowMap.getOrDefault(r,0)+1);
         }
         for (int i=0;i<grid.length;i++){
             int colArray[]=new int[grid.length];
-                for (int j=0;j<grid.length;j++){
-                    colArray[j]=grid[j][i];
-                }
-                count+=rowMap.getOrDefault(Arrays.toString(colArray),0);
+            for(int j=0;j<grid.length;j++){
+                colArray[j]=grid[j][i];
+            }
+            String c=Arrays.toString(colArray);
+            count=count+rowMap.getOrDefault(c,0);
         }
         return count;
     }
