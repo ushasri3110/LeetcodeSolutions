7497c08ca4cd6e431da07caccecf170ce50e8b0a
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-12-30 17:07:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (33.87%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -15,20 +15,20 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        List<Integer> L1=new ArrayList<>();
-        List<Integer> L2=new ArrayList<>();
-        leafNodes(root1,L1);
-        leafNodes(root2,L2);
-        return L1.equals(L2);
+        List<Integer> l1=new ArrayList<>();
+        List<Integer> l2=new ArrayList<>();
+        leafNodes(root1,l1);
+        leafNodes(root2,l2);
+        return l1.equals(l2);
     }
-    public void leafNodes(TreeNode root,List<Integer> L){
+    public void leafNodes(TreeNode root,List<Integer> l){
         if (root==null){
             return;
         }
         if (root.left==null && root.right==null){
-            L.add(root.val);
+            l.add(root.val);
         }
-        leafNodes(root.left,L);
-        leafNodes(root.right,L);
+        leafNodes(root.left,l);
+        leafNodes(root.right,l);
     }
 }
