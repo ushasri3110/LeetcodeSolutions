27c4ccaad4feb732c0ecd13c1dfbd3354d65aa16
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-01-16 18:28:56 +0530

    Time: 8 ms (8.85%) | Memory: 46.2 MB (54.97%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,34 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double median = 0;
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        int m = nums1.length;
-        int n = nums2.length;
-        int low = 0;
-        int high = m;
-        while (low <= high) {
-            int partitionX = (low + high) / 2;
-            int partitionY = (m + n + 1) / 2 - partitionX;
-            int maxX = partitionX == 0 ? Integer.MIN_VALUE : nums1[partitionX - 1];
-            int minX = partitionX == m ? Integer.MAX_VALUE : nums1[partitionX];
-            int maxY = partitionY == 0 ? Integer.MIN_VALUE : nums2[partitionY - 1];
-            int minY = partitionY == n ? Integer.MAX_VALUE : nums2[partitionY];
-            if (maxX <= minY && maxY <= minX) {
-                if ((m + n) % 2 == 0) {
-                    median = (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2.0;
-                    return median;
-                } else {
-                    median = Math.max(maxX, maxY);
-                    return median;
-                }
-            } else if (maxX > minY) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
+        int n=nums1.length;
+        int m=nums2.length;
+        int nums3[]=new int[n+m];
+        int i=0;
+        int j=0;
+        int k=0;
+        while (i<n && j<m){
+            if (nums1[i]<=nums2[j]){
+                nums3[k++]=nums1[i++];
+            }
+            else if (nums1[i]>nums2[j]){
+                nums3[k++]=nums2[j++];
             }
         }
-        return median;
+        while (i<n){
+            nums3[k++]=nums1[i++];
+        }
+        while (j<m){
+            nums3[k++]=nums2[j++];
+        }
+        int mid=(int)(m+n)/2;
+        System.out.println(mid);
+        return (m+n)%2==0?(nums3[mid]+nums3[mid-1])/2.0:nums3[mid]/1.0;
     }
 }
