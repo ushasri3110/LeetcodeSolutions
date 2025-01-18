f4cb03c357d5a5a17d413775bb5f861de9f00281
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-01-18 10:57:13 +0530

    Time: 3 ms (32.28%) | Memory: 68.9 MB (28.82%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,15 +1,14 @@
 class Solution {
     public int findCenter(int[][] edges) {
         int nodes=edges.length+1;
-        int indegree[]=new int[nodes];
-        int outdegree[]=new int[nodes];
-        for (int i=0;i<edges.length;i++){
-            outdegree[edges[i][0]-1]+=1;
-            indegree[edges[i][1]-1]+=1;
+        int degree[]=new int[nodes];
+        for (int[] row:edges){
+            degree[row[0]-1]++;
+            degree[row[1]-1]++;
         }
-        for (int j=0;j<nodes;j++){
-            if (outdegree[j]+indegree[j]==nodes-1){
-                return j+1;
+        for (int i=0;i<nodes;i++){
+            if (degree[i]==nodes-1){
+                return i+1;
             }
         }
         return -1;
