9db84ea7be6b50355963b3173d63bf6bfe7cab8a
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-02-08 22:12:39 +0530

    Time: 58 ms (86.99%) | Memory: 101.6 MB (25.34%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -0,0 +1,44 @@
+class NumberContainers {
+
+    public NumberContainers() {
+        numberToIndices = new HashMap<>();
+        indexToNumbers = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        indexToNumbers.put(index, number);
+
+        numberToIndices
+            .computeIfAbsent(number, k -> new PriorityQueue<>())
+            .add(index);
+    }
+
+    public int find(int number) {
+        if (!numberToIndices.containsKey(number)) {
+            return -1;
+        }
+
+        PriorityQueue<Integer> minHeap = numberToIndices.get(number);
+
+        while (!minHeap.isEmpty()) {
+            int index = minHeap.peek();
+
+            if (indexToNumbers.get(index) == number) {
+                return index;
+            }
+
+            minHeap.poll();
+        }
+
+        return -1;
+    }
+
+    private Map<Integer, PriorityQueue<Integer>> numberToIndices;
+    private Map<Integer, Integer> indexToNumbers;
+}
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
