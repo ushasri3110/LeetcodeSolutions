2c826e55a066c113eb7ea6934c92e743b4060b4f
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-03-28 08:02:10 +0530

    Time: 20 ms (25.14%) | Memory: 44 MB (64.09%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,25 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        Set<List<Integer>> result = new HashSet<>();
-        for (int i = 0; i < nums.length - 3; i++) {
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                int k = j + 1;
-                int l = nums.length - 1;
-                while (k < l) {
-                    long currentSum = (long)nums[i] + (long)nums[j] + (long)nums[k] + (long)nums[l];
-                    if (currentSum == (long) target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+        List<List<Integer>> result=new ArrayList<>();
+        int n=nums.length;
+        for (int i=0;i<n-3;i++){
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            for (int j=i+1;j<n-2;j++){
+                if (j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum= (long) nums[i]+nums[j]+nums[k]+nums[l];
+                    if (sum==target){
+                        result.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        while (k<l && nums[k]==nums[k+1]) k++;
+                        while (k<l && nums[l]==nums[l-1]) l--;
                         k++;
                         l--;
-                    } else if (currentSum > target) {
-                        l--;
-                    } else {
-                        k++;
                     }
+                    else if (sum<target) k++;
+                    else l--;
                 }
             }
         }
-        return new ArrayList<>(result);
+        return result;
     }
 }
