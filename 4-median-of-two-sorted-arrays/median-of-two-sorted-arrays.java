a2c83c90d487b14d7f55c187c21d6049b9c064d6
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-04-08 19:02:27 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (5.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,27 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
-        int nums3[]=new int[n+m];
-        int i=0;
-        int j=0;
-        int k=0;
-        while (i<n && j<m){
-            if (nums1[i]<=nums2[j]){
-                nums3[k++]=nums1[i++];
+        if (nums1.length>nums2.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int m=nums1.length;
+        int n=nums2.length;
+        int low=0;
+        int high=m;
+        while (low<=high){
+            int partitionX=(low+high)/2;
+            int partitionY=(m+n+1)/2-partitionX;
+            int maxLeftX=(partitionX==0) ? Integer.MIN_VALUE:nums1[partitionX-1];
+            int minRightX=(partitionX==m) ? Integer.MAX_VALUE:nums1[partitionX];
+            int maxLeftY=(partitionY==0)? Integer.MIN_VALUE:nums2[partitionY-1];
+            int minRightY=(partitionY==n)?Integer.MAX_VALUE:nums2[partitionY];
+            if (maxLeftX<=minRightY && minRightX>=maxLeftY){
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
             }
-            else if (nums1[i]>nums2[j]){
-                nums3[k++]=nums2[j++];
+            else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
             }
         }
-        while (i<n){
-            nums3[k++]=nums1[i++];
-        }
-        while (j<m){
-            nums3[k++]=nums2[j++];
-        }
-        int mid=(int)(m+n)/2;
-        System.out.println(mid);
-        return (m+n)%2==0?(nums3[mid]+nums3[mid-1])/2.0:nums3[mid]/1.0;
+        return -1.0;
     }
 }
