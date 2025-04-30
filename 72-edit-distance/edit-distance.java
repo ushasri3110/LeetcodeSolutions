59aafd9d95c6b6a3b5de6a4577a4fbb98708aa83
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-04-30 21:54:05 +0530

    Time: 5 ms (64.46%) | Memory: 45.2 MB (30.75%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,27 +1,22 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m=word1.length();
-        int n=word2.length();
-        int dp[][]=new int[m+1][n+1];
-        for (int i=1;i<=m;i++){
-            dp[i][0]=i;
+        int[][] dp=new int[word1.length()+1][word2.length()+1];
+        for (int i=0;i<word2.length()+1;i++){
+                dp[0][i]=i;
         }
-        for (int j=1;j<=n;j++){
-            dp[0][j]=j;
+        for (int j=0;j<word1.length()+1;j++){
+            dp[j][0]=j;
         }
-        for (int i=1;i<=m;i++){
-            for (int j=1;j<=n;j++){
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1];
+        for (int i=1;i<word1.length()+1;i++){
+            for (int j=1;j<word2.length()+1;j++){
+                if (word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
                 }
                 else{
-                    int topLeft = dp[i - 1][j - 1];
-                    int top = dp[i - 1][j];
-                    int left = dp[i][j - 1];
-                    dp[i][j] = Math.min(topLeft, Math.min(top, left)) + 1;
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
                 }
             }
         }
-        return dp[m][n];
+        return dp[word1.length()][word2.length()];
     }
 }
