05231effca7895c7b955bff6b95dd7ff1058d8d6
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-05-25 11:46:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (46.11%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,15 +15,11 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValidBSTHelper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+       return BSTHelper(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
-    public boolean isValidBSTHelper(TreeNode root,long min,long max){
-        if (root==null){
-            return true;
-        }
-        if (root.val<=min||root.val>=max){
-            return false;
-        }
-        return isValidBSTHelper(root.left,min,root.val)&&isValidBSTHelper(root.right,root.val,max);
+    public boolean BSTHelper(TreeNode root,long min,long max){
+        if (root==null) return true;
+        if (root.val <=min || root.val >= max) return false;
+        return BSTHelper(root.left,min,root.val) && BSTHelper(root.right,root.val,max);
     }
 }
