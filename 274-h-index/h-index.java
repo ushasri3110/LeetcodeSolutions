a2439be826acc945ec667693a22d66c63c324847
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-05-30 17:05:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (13.28%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,10 +1,19 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n=citations.length;
-        Arrays.sort(citations);
-        for (int i=0;i<n;i++){
-            int h=n-i;
-            if (citations[i]>=h) return h;
+        int[] buckets=new int[n+1];
+        for (int c:citations){
+            if (c>=n){
+                buckets[n]++;
+            }
+            else{
+                buckets[c]++;
+            }
+        }
+        int total=0;
+        for (int i=n;i>=0;i--){
+            total+=buckets[i];
+            if (total>=i) return i;
         }
         return 0;
     }
