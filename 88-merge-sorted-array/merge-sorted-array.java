825af72dfa00103026f5db7ea6c5695765996ee7
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-05-30 15:17:16 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (95.50%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,13 +3,16 @@ class Solution {
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
-        while (j>=0){
-            if (i>=0 && nums1[i]>nums2[j]){
+        while (i>=0 && j>=0){
+            if (nums1[i]>nums2[j]){
                 nums1[k--]=nums1[i--];
             }
             else{
                 nums1[k--]=nums2[j--];
             }
+        }
+        while (j>=0){
+            nums1[k--]=nums2[j--];
         }
     }
 }
