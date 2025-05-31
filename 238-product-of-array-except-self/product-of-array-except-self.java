7d231216caed97ed36ba41904ec435ccd4b6cf0a
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-05-31 20:46:09 +0530

    Time: 2 ms (88.98%) | Memory: 55.7 MB (36.95%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int after[]=new int[nums.length];
-        int before[]=new int[nums.length];
-        before[0]=1;
-        after[nums.length-1]=1;
-        for(int i=1;i<nums.length;i++){
-            before[i]=before[i-1]*nums[i-1];
+        int n=nums.length;
+        int[] prefix=new int[n];
+        int[] suffix=new int[n];
+        int[] result=new int[n];
+        prefix[0]=1;
+        suffix[n-1]=1;
+        for (int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
         }
-        for(int j=nums.length-2;j>=0;j--){
-            after[j]=after[j+1]*nums[j+1];
+        for (int i=n-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];
         }
-        int res[]=new int[nums.length];
-        for (int i=0;i<nums.length;i++){
-            res[i]=before[i]*after[i];
+        for (int i=0;i<n;i++){
+            result[i]=prefix[i]*suffix[i];
         }
-        return res;
+        return result;
     }
 }
