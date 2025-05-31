f92383835c69d5f7ea22d4ca94694c2c408cae49
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-05-31 10:12:45 +0530

    Time: 5 ms (87.65%) | Memory: 85.7 MB (66.73%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,34 +1,34 @@
-
 class Solution {
-    public boolean isVowel(char ch) {
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+    public boolean isVowel(char ch){
+        if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
             return true;
         }
         return false;
     }
-
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int res[] = new int[queries.length];
-        int prefixSum[] = new int[words.length];
-        int count=0;
-        for (int i = 0; i < words.length; i++) {
-            String current = words[i];
-            if (isVowel(current.charAt(0)) && isVowel(current.charAt(current.length() - 1))) {
-                count++;
+        int[] ps=new int[words.length];
+        for (int i=0;i<words.length;i++){
+            String current=words[i];
+            if (isVowel(current.charAt(0)) && isVowel(current.charAt(current.length()-1))){
+                ps[i]=i>0?ps[i-1]+1:1;
             }
-            prefixSum[i] = count;
+            else{
+                ps[i]=i>0?ps[i-1]:0;
+            }
         }
+        int[] res=new int[queries.length];
         for (int i=0;i<queries.length;i++){
+            int count=0;
             int start=queries[i][0];
             int end=queries[i][1];
-            if (start==0){
-                res[i]=prefixSum[end];
+            if (start>0){
+                count=ps[end]-ps[start-1];
             }
-            else {
-                res[i] = prefixSum[end] - prefixSum[start - 1];
+            else{
+                count=ps[end];
             }
+            res[i]=count;
         }
-
         return res;
     }
 }
