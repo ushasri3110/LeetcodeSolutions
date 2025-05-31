bd6285f5de259c62343c8ca904b2210aa72a0d0e
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-05-31 21:01:17 +0530

    Time: 3 ms (84.78%) | Memory: 46.2 MB (24.21%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -3,20 +3,16 @@ class Solution {
         int n=ratings.length;
         int[] candies=new int[n];
         Arrays.fill(candies,1);
-        int total=0;
-        for(int i=1;i<n;i++){
-            if (ratings[i]>ratings[i-1]){
-                candies[i]=candies[i-1]+1;
-            }
+        for (int i=1;i<n;i++){
+            if (ratings[i]>ratings[i-1]) candies[i]=candies[i-1]+1;
         }
-        for(int i=n-2;i>=0;i--){
-            if (ratings[i]>ratings[i+1]){
-                candies[i]=Math.max(candies[i],candies[i+1]+1);
-            }
+        for (int i=n-2;i>=0;i--){
+            if (ratings[i]>ratings[i+1]) candies[i]=Math.max(candies[i],candies[i+1]+1);
         }
+        int count=0;
         for (int i:candies){
-            total+=i;
+            count+=i;
         }
-        return total;
+        return count;
     }
 }
