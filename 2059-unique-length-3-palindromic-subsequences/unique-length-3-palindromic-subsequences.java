5b7aa9681e3f12f3cce76ea13f7ab11672838d17
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-01 16:40:19 +0530

    Time: 195 ms (64.92%) | Memory: 45.9 MB (20.64%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,23 +1,19 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int count=0;
-        int first[]=new int[26];
-        int last[]=new int[26];
+        int n = s.length();
+        int[] first=new int[26];
+        int[] last=new int[26];
         Arrays.fill(first,-1);
-
-        for (int i=0;i<s.length();i++){
-            int curr=s.charAt(i)-'a';
-            if (first[curr]==-1){
-                first[curr]=i;
-            }
-            last[curr]=i;
+        for (int i=0;i<n;i++){
+            int current=s.charAt(i)-'a';
+            if (first[current]==-1) first[current]=i;
+            last[current]=i;
         }
+        int count=0;
         for (int i=0;i<26;i++){
-            if (first[i]==-1){
-                continue;
-            }
+            if (first[i]==-1) continue;
             Set<Character> between=new HashSet<>();
-            for(int j=first[i]+1;j<last[i];j++){
+            for (int j=first[i]+1;j<last[i];j++){
                 between.add(s.charAt(j));
             }
             count+=between.size();
