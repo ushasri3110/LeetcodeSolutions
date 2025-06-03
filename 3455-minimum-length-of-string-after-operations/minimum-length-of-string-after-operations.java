31b1a5484d8356ba8f98cea6cd8dce34dc9dfc39
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-03 18:41:57 +0530

    Time: 10 ms (83.77%) | Memory: 46.2 MB (63.01%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.java
@@ -1,27 +1,15 @@
 class Solution {
     public int minimumLength(String s) {
-        int n=s.length();
-        if (n<3){
-            return n;
-        }
-        int freq[]=new int[26];
-        int minLength=0;
+        int[] freq=new int[26];
+        int totalLength=0;
         for (char ch:s.toCharArray()){
             freq[ch-'a']++;
         }
-        for (int f:freq){
-            if(f==0){
-                continue;
-            }
-            if (f%2==0){
-                minLength=minLength+2;
-            }
-            else{
-                minLength=minLength+1;
-            }
+        for (int i=0;i<26;i++){
+            if (freq[i]==0) continue;
+            if (freq[i]%2==0) totalLength+=2;
+            else totalLength+=1;
         }
-
-
-        return minLength;
+        return totalLength;
     }
 }
