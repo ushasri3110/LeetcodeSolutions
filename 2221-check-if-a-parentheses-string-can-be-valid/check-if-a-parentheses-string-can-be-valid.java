f99cd59c3eee58175482b03ca54b56c054ebb127
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-03 18:23:24 +0530

    Time: 14 ms (85.84%) | Memory: 45.6 MB (84.25%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -1,38 +1,21 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
         int n=s.length();
-        if (n%2==1){
-            return false;
-        }
-        Stack<Integer> unlocked=new Stack<>();
-        Stack<Integer> openBrackets=new Stack<>();
+        if (n%2!=0) return false;
+        int close=0,open=0,wildCard=0;
         for (int i=0;i<n;i++){
-            if (locked.charAt(i)=='0'){
-                unlocked.push(i);
-            }
-            else if (s.charAt(i)=='('){
-                openBrackets.push(i);
-            }
-            else if (s.charAt(i)==')'){
-                if (!openBrackets.isEmpty()){
-                    openBrackets.pop();
-                }
-                else if (!unlocked.isEmpty()){
-                    unlocked.pop();
-                }
-                else{
-                    return false;
-                }
-            }
+            if (locked.charAt(i)=='0') wildCard++;
+            else if (s.charAt(i)=='(') open++;
+            else close++;
+            if (wildCard+open<close) return false;
         }
-        while (!openBrackets.isEmpty() && !unlocked.isEmpty() && unlocked.peek()>openBrackets.peek()){
-            openBrackets.pop();
-            unlocked.pop();
-        }
-        if (!openBrackets.isEmpty()){
-            return false;
+        open=close=wildCard=0;
+        for (int i=n-1;i>=0;i--){
+            if (locked.charAt(i)=='0') wildCard++;
+            else if (s.charAt(i)=='(') open++;
+            else close++;
+            if (wildCard+close<open) return false;
         }
         return true;
-
     }
 }
