63738296fec280769c16b15aeea26c4823b83d5d
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-03 10:09:30 +0530

    Time: 15 ms (34.37%) | Memory: 54.9 MB (33.80%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] maxFreq=new int[26];
         List<String> res=new ArrayList<>();
-        int[] maxWordCount2 = new int[26];
-        for (String str:words2){
-            int[] tempCount = new int[26];
-            for (char ch : str.toCharArray()) {
-                tempCount[ch - 'a']++;
-            }
-            for (int i = 0; i < 26; i++) {
-                maxWordCount2[i] = Math.max(maxWordCount2[i], tempCount[i]);
+        for (String word:words2){
+            int[] freq=countFreq(word);
+            for (int i=0;i<26;i++){
+                maxFreq[i]=Math.max(maxFreq[i],freq[i]);
             }
         }
-        for (String a : words1) {
-            int[] wordCount1 = new int[26];
-            for (char ch : a.toCharArray()) {
-                wordCount1[ch - 'a']++;
-            }
-            
-            boolean isUniversal = true;
-            for (int i = 0; i < 26; i++) {
-                if (wordCount1[i] < maxWordCount2[i]) {
-                    isUniversal = false;
+        for (String word:words1){
+            int[] freq=countFreq(word);
+            boolean isUniversal=true;
+            for (int i=0;i<26;i++){
+                if (maxFreq[i]>freq[i]){
+                    isUniversal=false;
                     break;
                 }
             }
-            
-            if (isUniversal) {
-                res.add(a);
-            }
+            if (isUniversal) res.add(word);
         }
         return res;
+    }
+    private int[] countFreq(String word){
+        int[] freq=new int[26];
+        for (char ch:word.toCharArray()){
+            freq[ch-'a']++;
+        }
+        return freq;
     }
 }
