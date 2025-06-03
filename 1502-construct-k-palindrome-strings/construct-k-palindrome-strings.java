5f3b9c0bcedaad6710ade48a7da2361546735d04
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-03 10:22:56 +0530

    Time: 3 ms (99.37%) | Memory: 45.4 MB (51.88%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,21 +1,14 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-        if (s.length()<k){
-            return false;
-        }
-        if (s.length()==k){
-            return true;
-        }
-        int freq[]=new int[26];
-        int oddCount=0;
+        if (s.length()<k) return false;
+        int[] freq=new int[26];
         for (char ch:s.toCharArray()){
             freq[ch-'a']++;
         }
-        for (int i:freq){
-            if (i%2==1){
-                oddCount++;
-            }
+        int singles=0;
+        for (int i=0;i<26;i++){
+            if (freq[i]%2!=0) singles++;
         }
-        return oddCount<=k;
+        return singles<=k;
     }
 }
