08d89e06c9ea7ca0b51f4d5ce098c96e498ff5e1
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-04 16:26:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (86.69%) - LeetSync

--- 2509-minimize-xor/minimize-xor.java
+++ 2509-minimize-xor/minimize-xor.java
@@ -1,21 +1,28 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        //count number of set bits in num2
-        int c = Integer.bitCount(num2);
-        
-        int ans = 0;
-        //set all bits of num1 from left to right until c> 0
-        for(int i = 30;c>0 && i>=0 ;i--){
-            c -= ( (num1>>i) & 1);
-            ans |= (num1&(1<<i));
-        }
-        //if still c>0 fill the bits from left to right
-        for(int i = 0;c>0 ;i++){
-            if((ans&(1<<i)) == 0)  {
-                ans |= 1<<i;
-                c--;
+        int setBits=countSetBits(num2);
+        int res=0;
+        for (int i=31;i>=0 && setBits>0;i--){
+            if ((num1 & (1<<i))!=0){
+                setBits--;
+                res+=(1<<i);
             }
         }
-        return ans;
+        for (int i=0;i<32 && setBits>0;i++){
+            if ((num1 & (1<<i))==0){
+                setBits--;
+                res+=(1<<i);
+            }
+        }
+        return res;
+
+    }
+    private int countSetBits(int num){
+        int count=0;
+        while (num>0){
+            count+= (num & 1);
+            num = num>>1;
+        }
+        return count;
     }
 }
