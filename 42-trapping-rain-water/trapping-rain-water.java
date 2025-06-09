3145dcf5b593d4858e63acf1bc28dc79673469a8
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-09 20:20:12 +0530

    Time: 1 ms (62.38%) | Memory: 46.2 MB (67.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,30 +1,23 @@
 class Solution {
-    public int trap(int[] arr) {
-        int left=0;
-        int right=arr.length-1;
-        int leftMax=0;
-        int rightMax=0;
+    public int trap(int[] height) {
+        int n=height.length;
+        int[] prefix=new int[n];
+        int[] suffix=new int[n];
+        prefix[0]=height[0];
+        suffix[n-1]=height[n-1];
+        for (int i=1;i<n;i++){
+            prefix[i]=Math.max(prefix[i-1],height[i]);
+        }
+        for (int j=n-2;j>=0;j--){
+            suffix[j]=Math.max(suffix[j+1],height[j]);
+        }
         int totalWater=0;
-        while (left<right){
-            if (arr[left]<arr[right]){
-                if (arr[left]>=leftMax){
-                    leftMax=arr[left];
-                }
-                else{
-                    totalWater+=leftMax-arr[left];
-                }
-                left++;
-            }
-            else{
-                if(arr[right]>=rightMax){
-                    rightMax=arr[right];
-                }
-                else{
-                    totalWater+=rightMax-arr[right];
-                }
-                right--;
+        for (int i=0;i<n;i++){
+            if (height[i]<prefix[i] && height[i]<suffix[i]){
+                totalWater+=Math.min(prefix[i],suffix[i])-height[i];
             }
         }
+
         return totalWater;
     }
 }
