b6ed5b1153ab3da96b364438f50d04f929e50186
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-10 08:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (47.69%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -18,11 +18,10 @@ class Solution {
         if (root==null){
             return null;
         }
-        TreeNode temp=root.left;
-        root.left=root.right;
-        root.right=temp;
-        root.left=invertTree(root.left);
-        root.right=invertTree(root.right);
+        TreeNode left=invertTree(root.left);
+        TreeNode right=invertTree(root.right);
+        root.left=right;
+        root.right=left;
         return root;
     }
 }
