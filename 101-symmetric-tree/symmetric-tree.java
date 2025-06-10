fc515b9a1fbaae082996b4c5e41c895eb6b3b5d3
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-10 09:02:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (20.93%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,21 +15,12 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root==null){
-            return true;
-        }
-        return isSymmetricHelper(root.left,root.right);
+        if (root==null) return true;
+        return helper(root.left,root.right);
     }
-    public boolean isSymmetricHelper(TreeNode leftNode,TreeNode rightNode){
-        if (leftNode==null && rightNode==null){
-            return true;
-        }
-        if (leftNode==null || rightNode==null){
-            return false;
-        }
-        if (leftNode.val!=rightNode.val){
-            return false;
-        }
-        return isSymmetricHelper(leftNode.left,rightNode.right)&&isSymmetricHelper(leftNode.right,rightNode.left);
+    public boolean helper(TreeNode leftTree,TreeNode rightTree){
+        if (leftTree==null && rightTree==null) return true;
+        if (leftTree == null || rightTree == null || leftTree.val != rightTree.val) return false;
+        return helper(leftTree.left,rightTree.right) && helper(leftTree.right,rightTree.left);
     }
 }
