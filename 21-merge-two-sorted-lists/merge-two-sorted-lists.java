bf61a59c2044dc4af962627079ffbac361316211
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-10 13:03:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (24.26%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -26,18 +26,19 @@ class Solution {
                 list2=list2.next;
             }
         }
-        while (list1!=null){
-            ListNode newNode=new ListNode(list1.val);
-                temp.next=newNode;
-                temp=newNode;
-                list1=list1.next;
-        }
-        while (list2!=null){
-            ListNode newNode=new ListNode(list2.val);
-                temp.next=newNode;
-                temp=newNode;
-                list2=list2.next;
-        }
+        temp.next=list1==null?list2:list1;
+        // while (list1!=null){
+        //     ListNode newNode=new ListNode(list1.val);
+        //         temp.next=newNode;
+        //         temp=newNode;
+        //         list1=list1.next;
+        // }
+        // while (list2!=null){
+        //     ListNode newNode=new ListNode(list2.val);
+        //         temp.next=newNode;
+        //         temp=newNode;
+        //         list2=list2.next;
+        // }
         return head.next;
     }
 }
