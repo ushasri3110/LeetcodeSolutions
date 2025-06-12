54d3bfb881f78aaa1a1e3e58383316bdb51d0dbf
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-12 15:40:03 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (5.52%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,9 +15,20 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if (root==null){
-            return 0;
+        if (root==null) return 0;
+        int leftHeight=height(root,true);
+        int rightHeight=height(root,false);
+        if (leftHeight==rightHeight){
+            return (1<<leftHeight)-1;
         }
         return 1+countNodes(root.left)+countNodes(root.right);
+    }
+    public int height(TreeNode node,boolean isLeft){
+        int height=0;
+        while(node!=null){
+            height++;
+            node=isLeft?node.left:node.right;
+        }
+        return height;
     }
 }
