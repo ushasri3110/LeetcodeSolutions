4bc86d2001be802748aca9c090ef96616b659db9
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-13 11:14:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (49.77%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,28 +15,15 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        Map<TreeNode,Integer> map=new HashMap<>();
-        Stack<TreeNode> stack=new Stack();
-        int diameter=0;
-        if (root!=null){
-            stack.push(root);
-        }
-        while (!stack.isEmpty()){
-            TreeNode node = stack.peek();
-            if(node.left!=null && !map.containsKey(node.left)){
-                stack.push(node.left);
-            }
-            else if (node.right!=null && !map.containsKey(node.right)){
-                stack.push(node.right);
-            }
-            else{
-                stack.pop();
-                int leftDepth=map.getOrDefault(node.left,0);
-                int rightDepth=map.getOrDefault(node.right,0);
-                map.put(node,1+Math.max(leftDepth,rightDepth));
-                diameter=Math.max(diameter,leftDepth+rightDepth);
-            }
-        }
-        return diameter;
+        int[] diameter=new int[1];
+        height(root,diameter);
+        return diameter[0];
+    }
+    private int height(TreeNode root,int[] diameter){
+        if (root==null) return 0;
+        int lh=height(root.left,diameter);
+        int rh=height(root.right,diameter);
+        diameter[0]=Math.max(lh+rh,diameter[0]);
+        return 1+Math.max(rh,lh);
     }
 }
