056940bddb8873a3ac459e3e7ac2dbc54c769218
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-24 21:19:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (30.26%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -3,20 +3,12 @@ class Solution {
         int i=0;
         int j=0;
         StringBuilder sb=new StringBuilder();
-        while(i<word1.length()&&j<word2.length()){
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(j));
-            i++;
-            j++;
+        while (i<word1.length() && j<word2.length()){
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
         }
-        while(i<word1.length()){
-            sb.append(word1.charAt(i));
-            i++;
-        }
-        while(j<word2.length()){
-            sb.append(word2.charAt(j));
-            j++;
-        }
+        if (i!=word1.length()) sb.append(word1.substring(i));
+        if (j!=word2.length()) sb.append(word2.substring(j));
         return sb.toString();
     }
 }
