85e15cf2080493774ece34ea7b6e9788c24e9cd2
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-26 18:31:49 +0530

    Time: 31 ms (96.84%) | Memory: 66.2 MB (95.00%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,27 +1,21 @@
 class Solution {
     public int minReorder(int n, int[][] connections) {
-        List<List<int[]>> graph=new ArrayList<>();
-        for (int i=0;i<n;i++){
-            graph.add(new ArrayList<>());
+        List<int[]>[] adj=new List[n];
+        for (int i=0;i<n;i++) adj[i]=new ArrayList<>();
+        for (int[] con:connections){
+            adj[con[0]].add(new int[]{con[1],1});
+            adj[con[1]].add(new int[]{con[0],0});
         }
-        for (int[] c:connections){
-            int from=c[0];
-            int to=c[1];
-            graph.get(from).add(new int[]{to,1});
-            graph.get(to).add(new int[]{from,0});
-        }
-        boolean visited[]=new boolean[n];
-        return dfs(graph,visited,0);
+        return dfs(adj,new boolean[n],0);
     }
-    public int dfs(List<List<int[]>> graph,boolean[] visited,int current){
-        visited[current]=true;
+    private int dfs(List<int[]>[] adj,boolean[] visited,int from){
         int count=0;
-        for(int[] neighbour:graph.get(current)){
-            int nextCity=neighbour[0];
-            int direction=neighbour[1];
-            if (!visited[nextCity]){
-                count+=direction;
-                count+=dfs(graph,visited,nextCity);
+        visited[from]=true;
+        for (int[] i:adj[from]){
+            int next=i[0];
+            int c=i[1];
+            if (!visited[next]){
+                count+=c+dfs(adj,visited,next);
             }
         }
         return count;
