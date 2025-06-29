ac16962cd241471a32095a3798a93aae5c0935d9
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-29 17:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (96.25%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,35 +1,33 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low = 0, high = nums.length - 1;
+        int n=nums.length;
+        int low = 0, high = n- 1;
 
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            boolean leftEqual = mid > 0 && nums[mid] == nums[mid - 1];
-            boolean rightEqual = mid < nums.length - 1 && nums[mid] == nums[mid + 1];
-
-            if (!leftEqual && !rightEqual) {
-                return nums[mid]; // Found the single element
-            }
-
-            // Determine the correct half to search
-            if (leftEqual) {
-                int leftCount = mid - 1 - low + 1;
-                if (leftCount % 2 != 0) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 2;
+            int mid=(low+high)/2;
+            boolean leftEqual= mid>0 && nums[mid]==nums[mid-1];
+            boolean rightEqual=mid<n-1 && nums[mid]==nums[mid+1];
+            if (!leftEqual && !rightEqual) return nums[mid];
+            if (leftEqual){
+                int leftCount=mid-1+low+1;
+                if (leftCount%2==0){
+                    high=mid-2;
                 }
-            } else if (rightEqual) {
-                int rightCount = high - (mid + 1) + 1;
-                if (rightCount % 2 != 0) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 2;
+                else{
+                    low=mid+1;
+                }
+            }
+            else if(rightEqual){
+                int rightCount=high+1-(mid+1);
+                if (rightCount%2==0){
+                    low=mid+2;
+                }
+                else{
+                    high=mid-1;
                 }
             }
         }
 
-        return -1; // Should never reach here if input is valid
+        return -1;
     }
 }
