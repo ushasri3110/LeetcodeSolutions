d6bfef50aa168594e5ed0b312e05a09cbec8a0b5
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-06-29 17:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (90.68%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -2,17 +2,34 @@ class Solution {
     public int singleNonDuplicate(int[] nums) {
         int low = 0, high = nums.length - 1;
 
-        while (low < high) {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (mid % 2 == 1) {
-                mid--;
+
+            boolean leftEqual = mid > 0 && nums[mid] == nums[mid - 1];
+            boolean rightEqual = mid < nums.length - 1 && nums[mid] == nums[mid + 1];
+
+            if (!leftEqual && !rightEqual) {
+                return nums[mid]; // Found the single element
             }
-            if (nums[mid] == nums[mid + 1]) {
-                low = mid + 2;
-            } else {
-                high = mid;
+
+            // Determine the correct half to search
+            if (leftEqual) {
+                int leftCount = mid - 1 - low + 1;
+                if (leftCount % 2 != 0) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 2;
+                }
+            } else if (rightEqual) {
+                int rightCount = high - (mid + 1) + 1;
+                if (rightCount % 2 != 0) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 2;
+                }
             }
         }
-        return nums[low];
+
+        return -1; // Should never reach here if input is valid
     }
 }
