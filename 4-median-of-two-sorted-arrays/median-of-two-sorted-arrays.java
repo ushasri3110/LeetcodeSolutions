41bed8c6a3ef843247fe668b0961ca6c2d31fd79
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-07-01 22:29:38 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (5.46%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,31 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length>nums2.length){
-            return findMedianSortedArrays(nums2,nums1);
-        }
-        int m=nums1.length;
-        int n=nums2.length;
-        int low=0;
-        int high=m;
+        if (nums1.length>nums2.length) return findMedianSortedArrays(nums2,nums1);
+        int m=nums1.length, n=nums2.length;
+        int low=0, high=m;
         while (low<=high){
-            int partitionX=(low+high)/2;
-            int partitionY=(m+n+1)/2-partitionX;
-            int maxLeftX=(partitionX==0) ? Integer.MIN_VALUE:nums1[partitionX-1];
-            int minRightX=(partitionX==m) ? Integer.MAX_VALUE:nums1[partitionX];
-            int maxLeftY=(partitionY==0)? Integer.MIN_VALUE:nums2[partitionY-1];
-            int minRightY=(partitionY==n)?Integer.MAX_VALUE:nums2[partitionY];
-            if (maxLeftX<=minRightY && minRightX>=maxLeftY){
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
-                } else {
-                    return Math.max(maxLeftX, maxLeftY);
+            int i=(low+high)/2;
+            int j=(m+n+1)/2-i;
+            int left1=(i==0)?Integer.MIN_VALUE:nums1[i-1];
+            int left2=(j==0)?Integer.MIN_VALUE:nums2[j-1];
+            int right1=(i==m)?Integer.MAX_VALUE:nums1[i];
+            int right2=(j==n)?Integer.MAX_VALUE:nums2[j];
+            if (left1<=right2 && left2<=right1){
+                if ((m+n)%2==0){
+                    return (Math.max(left1,left2)+Math.min(right1,right2))/2.0;
+                }
+                else{
+                    return Math.max(left1,left2);
                 }
             }
-            else if (maxLeftX > minRightY) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
+            else if (left1>right2){
+                high=i-1;
             }
+            else low=i+1;
         }
         return -1.0;
     }
