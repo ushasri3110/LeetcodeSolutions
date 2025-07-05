e27404d84e53b111f9b3db5d3186a0be462bf6c7
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-07-05 16:07:29 +0530

    Time: 12 ms (45.28%) | Memory: 46 MB (19.27%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -1,50 +1,22 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        char prev=' ';
-        int dCount=0;
-        int rCount=0;
-        int nextRBanned=0;
-        int nextDBanned=0;
-        Queue<Character> queue=new LinkedList<>();
-        for (char ch:senate.toCharArray()){
-            if (ch=='R'){
-                rCount++;
+        int n=senate.length();
+        Queue<Integer> rq=new LinkedList<>();
+        Queue<Integer> dq=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if (senate.charAt(i)=='R') rq.offer(i);
+            else dq.offer(i);
+        }
+        while (!rq.isEmpty() && !dq.isEmpty()){
+            int rIndex=rq.poll();
+            int dIndex=dq.poll();
+            if (rIndex<dIndex){
+                rq.offer(rIndex+n);
             }
             else{
-                dCount++;
-            }
-            queue.offer(ch);
-        }
-        if (dCount==0){
-            return "Radiant";
-        }
-        else if (rCount==0){
-            return "Dire";
-        }
-
-        while(rCount>0 && dCount>0){
-            prev=queue.poll();
-            if (prev=='R'){
-                if (nextRBanned==0){
-                    nextDBanned++;
-                    queue.offer(prev);
-                }
-                else{
-                    rCount--;
-                    nextRBanned--;
-                }
-            }
-            else{
-                if(nextDBanned==0){
-                    nextRBanned++;
-                    queue.offer(prev);
-                }
-                else{
-                    dCount--;
-                    nextDBanned--;
-                }
+                dq.offer(dIndex+n);
             }
         }
-        return queue.peek()=='R'?"Radiant":"Dire";
+        return rq.isEmpty()?"Dire":"Radiant";
     }
 }
