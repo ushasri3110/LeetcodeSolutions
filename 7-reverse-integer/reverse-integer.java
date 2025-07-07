e5c19fb0bf6a248bd0f9c7b7ef107f3b3071e9a2
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-07-07 15:48:21 +0530

    Time: 13 ms (4.41%) | Memory: 41.2 MB (34.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,9 +3,11 @@ class Solution {
         int ans=0;
         while (x!=0){
             int rem=x%10;
-            if (ans>Integer.MAX_VALUE/10 || ans==Integer.MAX_VALUE || ans==Integer.MIN_VALUE || ans<Integer.MIN_VALUE/10){
-                return 0;
-            }
+            System.out.println(rem);
+            if (ans > Integer.MAX_VALUE / 10 || 
+               (ans == Integer.MAX_VALUE / 10 && rem > 7)) return 0;
+            if (ans < Integer.MIN_VALUE / 10 || 
+               (ans == Integer.MIN_VALUE / 10 && rem < -8)) return 0;
             ans=ans*10+rem;
             x=x/10;
         }
