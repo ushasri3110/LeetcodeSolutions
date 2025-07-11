3b54174e11b2c0d9db961d10e30d2274a0cd861b
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-07-11 19:31:52 +0530

    Time: 2 ms (99.71%) | Memory: 50.7 MB (17.04%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,50 +2,36 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res=new ArrayList<>();
         int n=nums.length;
-        if (n==0){
-            return res;
-        }
-        int candidate1=Integer.MIN_VALUE;
-        int candidate2=Integer.MIN_VALUE;
-        int count1=0;
-        int count2=0;
+        int c1=0,c2=0;
+        int m1=Integer.MIN_VALUE,m2=Integer.MIN_VALUE;
         for (int i:nums){
-            if (i==candidate1){
-                count1++;
+            if (c1==0 && i!=m2){
+                c1=1;
+                m1=i;
             }
-            else if (i==candidate2){
-                count2++;
+            else if (c2==0 && i!=m1){
+                c2=1;
+                m2=i;
             }
-            else if (count1==0){
-                candidate1=i;
-                count1=1;
+            else if (i==m1){
+                c1++;
             }
-            else if (count2==0){
-                candidate2=i;
-                count2=1;
+            else if (i==m2){
+                c2++;
             }
             else{
-                count1--;
-                count2--;
+                c1--;
+                c2--;
             }
         }
-        count1=0;
-        count2=0;
+        c1=0;
+        c2=0;
         for (int i:nums){
-            if (i==candidate1){
-                count1++;
-            }
-            if (i==candidate2){
-                count2++;
-            }
+            if (i==m1) c1++;
+            if (i==m2) c2++;
         }
-        System.out.println(count1+" "+count2);
-        if (count1>n/3){
-            res.add(candidate1);
-        }
-        if (count2>n/3){
-            res.add(candidate2);
-        }
+        if (c1>(n/3)) res.add(m1);
+        if (c2>(n/3)) res.add(m2);
         return res;
     }
 }
