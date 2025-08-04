5f94ec284950b33f96a366d2ad2429f0b602ebb2
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-04 10:43:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (74.02%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -9,27 +9,58 @@
  * }
  */
 class Solution {
+    // private ListNode findKth(ListNode head,int k){
+    //     ListNode current=head;
+    //     while (current!=null && k>0){
+    //         k--;
+    //         current=current.next;
+    //     }
+    //     return current;
+    // }
+    // public ListNode reverseKGroup(ListNode head, int k) {
+    //     if (head==null || k==1) return head;
+    //     ListNode dummy=new ListNode(0);
+    //     dummy.next=head;
+    //     ListNode prevGroupEnd=dummy;
+    //     while (true){
+    //         ListNode kth=findKth(prevGroupEnd,k);
+    //         if (kth==null) break;
+    //         ListNode groupStart=prevGroupEnd.next;
+    //         ListNode nextGroupStart=kth.next;
+    //         ListNode current=groupStart,prev=nextGroupStart,next;
+    //         while (current!=nextGroupStart){
+    //             next=current.next;
+    //             current.next=prev;
+    //             prev=current;
+    //             current=next;
+    //         }
+    //         prevGroupEnd.next=kth;
+    //         prevGroupEnd=groupStart;
+    //     }
+    //     return dummy.next;
+    // }
     private ListNode findKth(ListNode head,int k){
-        ListNode current=head;
-        while (current!=null && k>0){
+        ListNode temp=head;
+        while (temp!=null && k>0){
+            temp=temp.next;
             k--;
-            current=current.next;
         }
-        return current;
+        return temp;
     }
-    public ListNode reverseKGroup(ListNode head, int k) {
+    public ListNode reverseKGroup(ListNode head, int k){
         if (head==null || k==1) return head;
         ListNode dummy=new ListNode(0);
         dummy.next=head;
         ListNode prevGroupEnd=dummy;
         while (true){
+            ListNode groupStart=prevGroupEnd.next;
             ListNode kth=findKth(prevGroupEnd,k);
             if (kth==null) break;
-            ListNode groupStart=prevGroupEnd.next;
             ListNode nextGroupStart=kth.next;
-            ListNode current=groupStart,prev=nextGroupStart,next;
+            ListNode current=groupStart;
+            ListNode prev=nextGroupStart;
             while (current!=nextGroupStart){
-                next=current.next;
+                ListNode next=current.next;
                 current.next=prev;
                 prev=current;
                 current=next;
@@ -39,4 +70,5 @@ class Solution {
         }
         return dummy.next;
     }
+
 }
