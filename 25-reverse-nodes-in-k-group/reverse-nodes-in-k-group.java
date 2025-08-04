e70f05dd2ad068314f4ff49d9b21b05fa325db69
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-04 10:43:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (87.72%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -9,36 +9,6 @@
  * }
  */
 class Solution {
-    // private ListNode findKth(ListNode head,int k){
-    //     ListNode current=head;
-    //     while (current!=null && k>0){
-    //         k--;
-    //         current=current.next;
-    //     }
-    //     return current;
-    // }
-    // public ListNode reverseKGroup(ListNode head, int k) {
-    //     if (head==null || k==1) return head;
-    //     ListNode dummy=new ListNode(0);
-    //     dummy.next=head;
-    //     ListNode prevGroupEnd=dummy;
-    //     while (true){
-    //         ListNode kth=findKth(prevGroupEnd,k);
-    //         if (kth==null) break;
-    //         ListNode groupStart=prevGroupEnd.next;
-    //         ListNode nextGroupStart=kth.next;
-    //         ListNode current=groupStart,prev=nextGroupStart,next;
-    //         while (current!=nextGroupStart){
-    //             next=current.next;
-    //             current.next=prev;
-    //             prev=current;
-    //             current=next;
-    //         }
-    //         prevGroupEnd.next=kth;
-    //         prevGroupEnd=groupStart;
-    //     }
-    //     return dummy.next;
-    // }
     private ListNode findKth(ListNode head,int k){
         ListNode temp=head;
         while (temp!=null && k>0){
