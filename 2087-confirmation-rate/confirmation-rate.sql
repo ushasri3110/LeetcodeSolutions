c58fc1c77bfdf92ea1a93d1e5e183a455550351d
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-10 19:48:04 +0530

    Time: 812 ms (28.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+SELECT s.user_id,
+ROUND(
+    AVG(CASE WHEN c.action='confirmed' THEN 1 ELSE 0 END),
+    2
+) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
