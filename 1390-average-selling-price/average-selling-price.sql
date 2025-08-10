b1c1dd771996b31f880ad335e026e41dbbdbf279
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-10 20:16:05 +0530

    Time: 808 ms (47.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+SELECT p.product_id,
+ROUND(COALESCE(SUM(p.price*u.units)/SUM(u.units),0),2) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id=u.product_id 
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
