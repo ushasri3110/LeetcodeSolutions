b404aa5fa8a83ef41f7dd6ee6c19ac12aaeb5109
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-10 15:33:46 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (44.54%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -6,23 +6,25 @@ class Solution {
         int low=0;
         int high=m;
         while (low<=high){
-            int i=(low+high)/2;
+            int i= (low+high)/2;
             int j=(m+n+1)/2-i;
             int left1=i==0?Integer.MIN_VALUE:nums1[i-1];
-            int right1=i==m?Integer.MAX_VALUE:nums1[i];
             int left2=j==0?Integer.MIN_VALUE:nums2[j-1];
+            int right1=i==m?Integer.MAX_VALUE:nums1[i];
             int right2=j==n?Integer.MAX_VALUE:nums2[j];
             if (left1<=right2 && left2<=right1){
                 if ((m+n)%2==0){
                     return (Math.max(left1,left2)+Math.min(right1,right2))/2.0;
                 }
-                else return Math.max(left1,left2);
+                else{
+                    return Math.max(left1,left2)*1.0;
+                }
             }
-            else if (left1>right2){
-                high=i-1;
+            else if (right1<left2){
+                low=i+1;
             }
             else{
-                low=i+1;
+                high=i-1;
             }
         }
         return -1.0;
