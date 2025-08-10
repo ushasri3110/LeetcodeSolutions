cbc39cc2f746d7d9183a3389a27fbaef5b16a650
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-10 16:33:19 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (62.80%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int m = s.length();
-        int n = p.length();
-        boolean[][] dp = new boolean[m + 1][n + 1];
-        dp[0][0] = true;
-        for (int j = 2; j <= n; j = j + 2) {
-            if (p.charAt(j - 1) == '*') {
-                dp[0][j] = dp[0][j - 2];
-            }
+        int m=s.length();
+        int n=p.length();
+        boolean[][] dp=new boolean[m+1][n+1];
+        dp[0][0]=true;
+        for (int j=2;j<=n;j=j+2){
+            if (p.charAt(j-1)=='*') dp[0][j]=dp[0][j-2];
         }
         for (int i=1;i<=m;i++){
             for (int j=1;j<=n;j++){
@@ -17,9 +15,9 @@ class Solution {
                     dp[i][j]=dp[i-1][j-1];
                 }
                 else if (pc=='*'){
-                    char prevPc=p.charAt(j-2);
-                    if (prevPc=='.' || prevPc==sc){
-                        dp[i][j]=dp[i][j-2] || dp[i-1][j];
+                    char prevChar=p.charAt(j-2);
+                    if (prevChar=='.' || prevChar==sc){
+                        dp[i][j]=dp[i-1][j] || dp[i][j-2];
                     }
                     else{
                         dp[i][j]=dp[i][j-2];
