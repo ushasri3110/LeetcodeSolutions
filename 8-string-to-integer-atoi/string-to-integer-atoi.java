1dcbbb45452bf48f63433fbd8da8827454c63fc5
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-10 16:18:39 +0530

    Time: 2 ms (24.29%) | Memory: 42.9 MB (14.97%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,28 +1,26 @@
 class Solution {
     public int myAtoi(String s) {
         int res=0;
-        s=s.trim();
-        if (s.length()==0){
-            return 0;
-        }
+        int n=s.length();
         int i=0;
+        s=s.trim();
+        if (s.length()<1) return 0;
         boolean isNegative=s.charAt(0)=='-';
-        if (s.charAt(i)=='+'||s.charAt(i)=='-'){
+        if (s.charAt(0)=='-' || s.charAt(0)=='+'){
             i++;
         }
         while (i<s.length()){
-            char current=s.charAt(i);
-            if (Character.isDigit(current)){
-                if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && current - '0' > Integer.MAX_VALUE % 10)) {
-                    return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-                }
-                res=res*10+(current-'0');
-            }
-            else{
+            char ch=s.charAt(i);
+            if (!Character.isDigit(ch)){
                 break;
             }
+            int digit=ch-'0';
+            if (res>Integer.MAX_VALUE/10 || (res==Integer.MAX_VALUE/10 && digit>Integer.MAX_VALUE%10)){
+                return isNegative?Integer.MIN_VALUE:Integer.MAX_VALUE;
+            }
+            res=res*10+digit;
             i++;
         }
-        return isNegative?res*-1:res;
+        return isNegative?-res:res;
     }
 }
