29b2a030e43810d03bb36e89c0d5a5bc3c70e44f
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-12 15:45:29 +0530

    Time: 14 ms (57.58%) | Memory: 46.9 MB (90.89%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -2,27 +2,32 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
+        int[][] directions={{-1,0},{1,0},{0,-1},{0,1}};
         int[][] res = new int[m][n];
+        Queue<int[]> q=new LinkedList<>();
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                res[i][j] = 100000;
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    res[i][j] = 0;
-                } else {
-                    if (i>0) res[i][j]=Math.min(res[i][j],1+res[i-1][j]);
-                    if (j>0) res[i][j]=Math.min(res[i][j],1+res[i][j-1]);
+                if (mat[i][j]==0){
+                    res[i][j]=0;
+                    q.offer(new int[]{i,j,0});
+                }
+                else{
+                    res[i][j] = 100000;
                 }
             }
         }
-        for (int i=m-1;i>=0;i--){
-            for (int j=n-1;j>=0;j--){
-                if (mat[i][j]!=0){
-                    if (i<m-1) res[i][j]=Math.min(res[i][j],1+res[i+1][j]);
-                    if (j<n-1) res[i][j]=Math.min(res[i][j],1+res[i][j+1]);
+        while (!q.isEmpty()){
+            int[] top=q.poll();
+            int r=top[0];
+            int c=top[1];
+            int d=top[2];
+            for (int[] dir:directions){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+                if (nr>=0 && nc>=0 && nr<m && nc<n && mat[nr][nc]!=0){
+                    mat[nr][nc]=0;
+                    res[nr][nc]=d+1;
+                    q.offer(new int[]{nr,nc,d+1});
                 }
             }
         }
