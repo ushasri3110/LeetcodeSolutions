360a6e216210996cdc8ff460401f4dfda85080eb
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-08-12 16:25:54 +0530

    Time: 71 ms (76.33%) | Memory: 45.9 MB (84.35%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -7,36 +7,34 @@ class Pair {
         this.length = length;
     }
 }
-
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<>(wordList);
-        if (!wordSet.contains(endWord)) return 0;
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(beginWord, 1));
-        while (!q.isEmpty()) {
-            Pair top = q.poll();
-            String current = top.word;
-            int currentLength = top.length;
-            char[] wordChars = current.toCharArray();
-            for (int pos = 0; pos < wordChars.length; pos++) {
-                char originalChar = wordChars[pos];
-                for (char c = 'a'; c <= 'z'; c++) {
-                    if (c == originalChar) continue; 
-                    wordChars[pos] = c;
-                    String newWord = new String(wordChars);
-                    if (newWord.equals(endWord)) {
-                        return currentLength + 1;
-                    }
-                    if (wordSet.contains(newWord)) {
-                        q.offer(new Pair(newWord, currentLength + 1));
-                        wordSet.remove(newWord); 
+        Set<String> set=new HashSet<>(wordList);
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair(beginWord,1));
+        while(!q.isEmpty()){
+            Pair top=q.poll();
+            String current=top.word;
+            int currentLength=top.length;
+            char[] wordChar=current.toCharArray();
+            for (int i=0;i<wordChar.length;i++){
+                char originalChar=wordChar[i];
+                for (char ch='a';ch<='z';ch++){
+                    if (originalChar==ch) continue;
+                    wordChar[i]=ch;
+                    String newWord=new String(wordChar);
+                    if (newWord.equals(endWord)) return currentLength+1;
+                    if (set.contains(newWord)){
+                        q.offer(new Pair(newWord,currentLength+1));
+                        set.remove(newWord);
                     }
                 }
-                wordChars[pos] = originalChar;
+                wordChar[i]=originalChar;
             }
         }
-
-        return 0; 
+        return 0;
     }
 }
