5d0843c2b25f79470fdc3bc7d3b2b4e05e61081d
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-09-02 19:06:16 +0530

    Time: 496 ms (18.98%) | Memory: 75.6 MB (94.36%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
@@ -5,20 +5,20 @@ class Solution {
             pq.offer(c);
         }
         while (extraStudents>0){
-            int[] cur=pq.poll();
-            cur[0]++;
-            cur[1]++;
-            pq.offer(cur);
+            int[] c=pq.poll();
+            c[0]++;
+            c[1]++;
+            pq.offer(c);
             extraStudents--;
         }
         double res=0.0;
         while (!pq.isEmpty()){
-            int[] cur=pq.poll();
-            res+=(double) cur[0]/cur[1];
+            int[] c=pq.poll();
+            res+= (double) c[0]/c[1];
         }
         return res/classes.length;
     }
     public double gain(int[] c){
-        return (double) (c[0]+1)/(c[1]+1)- (double)c[0]/c[1];
+        return (double) (c[0]+1)/(c[1]+1) - (double) c[0]/c[1];
     }
 }
