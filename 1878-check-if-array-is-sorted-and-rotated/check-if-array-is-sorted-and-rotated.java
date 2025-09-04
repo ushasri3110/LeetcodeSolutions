3d7bf0fa96cd5b0f7dcb9bfef26aeb45b2638431
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-09-04 09:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (93.28%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,9 +3,10 @@ class Solution {
         int n=nums.length;
         int count=0;
         for (int i=0;i<n;i++){
-            if (nums[i]>nums[(i+1)%n]) count++;
-            if (count>1) return false;
+            if (nums[i]>nums[(i+1)%n]){
+                count++;
+            }
         }
-        return true;
+        return count<=1;
     }
 }
