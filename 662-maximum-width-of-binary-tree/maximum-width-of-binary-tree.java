fc03043cd04c30b90fd3c0b2c7b9d77520e13e2b
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-09-05 21:20:53 +0530

    Time: 1 ms (99.11%) | Memory: 43.4 MB (65.73%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -23,25 +23,22 @@ class Pair{
 }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root==null) return 0;
+        int maxWidth=0;
         Queue<Pair> q=new LinkedList<>();
         q.offer(new Pair(root,0));
-        int ans=0;
         while (!q.isEmpty()){
             int size=q.size();
             int first=0;
             int last=0;
             for (int i=0;i<size;i++){
-                TreeNode front=q.peek().node;
-                int curIndex=q.peek().index;
-                q.poll();
-                if (i == 0) first = curIndex;
-                if (i == size - 1) last = curIndex;
-                if (front.left!=null) q.offer(new Pair(front.left,2*curIndex+1));
-                if (front.right!=null) q.offer(new Pair(front.right,2*curIndex+2));
+                Pair current=q.poll();
+                if (i==0) first=current.index;
+                if (i==size-1) last=current.index;
+                if (current.node.left!=null) q.offer(new Pair(current.node.left,2*current.index+1));
+                if (current.node.right!=null) q.offer(new Pair(current.node.right,2*current.index+2));
             }
-            ans=Math.max(ans,last-first+1);
+            maxWidth=Math.max(maxWidth,last-first+1);
         }
-        return ans;
+        return maxWidth;
     }
 }
