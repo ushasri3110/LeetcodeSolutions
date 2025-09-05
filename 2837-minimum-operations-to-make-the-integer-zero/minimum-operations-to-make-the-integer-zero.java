30a369610840cb64d85f77c35558231c1a34c7ca
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-09-05 19:59:15 +0530

    Time: 1 ms (38.46%) | Memory: 40.8 MB (64.62%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -1,12 +1,15 @@
 class Solution {
     public int makeTheIntegerZero(int num1, int num2) {
-        for (int i=1;i<=60;i++){
-            long x= (long)num1- (long) i*num2;
-            if (x<0) continue;
-             if (Long.bitCount(x) <= i && x >= i) {
-                return i;
+        int k = 1;
+        while (true) {
+            long x = num1 - (long) num2 * k;
+            if (x < k) {
+                return -1;
             }
+            if (k >= Long.bitCount(x)) {
+                return k;
+            }
+            k++;
         }
-        return -1;
     }
 }
