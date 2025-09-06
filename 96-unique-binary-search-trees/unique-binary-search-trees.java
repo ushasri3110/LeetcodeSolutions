eae4af9ab4a300f195232922010f84b3814a7cf2
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-09-06 09:45:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (79.73%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for (int nodes=2;nodes<=n;nodes++){
+            for (int root=1;root<=nodes;root++){
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+        return dp[n];
+    }
+}
