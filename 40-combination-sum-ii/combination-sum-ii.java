2a55c5d9ec2939c63a076eefef04d4cf8a022e43
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-09-27 16:17:55 +0530

    Time: 2 ms (99.80%) | Memory: 43.3 MB (73.30%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -2,25 +2,20 @@ class Solution {
     List<List<Integer>> res=new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        combinationSumHelper(0,new ArrayList<>(),0,candidates,target);
+        combinationHelper(0,candidates,target,new ArrayList<>());
         return res;
     }
-    public void combinationSumHelper(int index,List<Integer> current,int sum,int[] candidates,int target){
-        if (sum > target) return;
-        if (index==candidates.length){
-            if (sum==target){
-                res.add(new ArrayList<>(current));
-            }
+    public void combinationHelper(int idx,int[] nums,int target,List<Integer> c){
+        if (target==0){
+            res.add(new ArrayList<>(c));
             return;
         }
-        current.add(candidates[index]);
-        sum+=candidates[index];
-        combinationSumHelper(index+1,current,sum,candidates,target);
-        current.remove(current.size()-1);
-        sum-=candidates[index];
-        while (index+1 < candidates.length && candidates[index]==candidates[index+1]){
-            index++;
+        for (int i=idx;i<nums.length;i++){
+            if (i>idx && nums[i]==nums[i-1]) continue;
+            if (nums[i]>target) break;
+            c.add(nums[i]);
+            combinationHelper(i+1,nums,target-nums[i],c);
+            c.remove(c.size()-1);
         }
-        combinationSumHelper(index+1,current,sum,candidates,target);
     }
 }
