25a020425a6986146748cad435e0c88020085b9e
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2025-10-01 09:56:36 +0530

    Time: 384 ms (5.11%) | Memory: 41.1 MB (59.61%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,19 +1,18 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        boolean s = solveSudokuHelper(board);
+        solve(board);
     }
-
-    public boolean solveSudokuHelper(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char ch = '1'; ch <= '9'; ch++) {
-                        if (isValid(board, i, j, ch)) {
-                            board[i][j] = ch;
-                            if (solveSudokuHelper(board)) {
+    private boolean solve(char[][] board){
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                if (board[i][j]=='.'){
+                    for (char ch='1';ch<='9';ch++){
+                        if (isValid(i,j,ch,board)){
+                            board[i][j]=ch;
+                            if (solve(board)){
                                 return true;
-                            } 
-                            board[i][j] = '.';
+                            }
+                            board[i][j]='.';
                         }
                     }
                     return false;
@@ -22,18 +21,11 @@ class Solution {
         }
         return true;
     }
-
-    public boolean isValid(char[][] board, int row, int col, char ch) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == ch) {
-                return false;
-            }
-            if (board[i][col] == ch) {
-                return false;
-            }
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch) {
-                return false;
-            }
+    private boolean isValid(int row,int col,char ch,char[][] board){
+        for (int i=0;i<9;i++){
+            if (board[i][col]==ch) return false;
+            if (board[row][i]==ch) return false;
+            if (board[3* (row/3) +i/3][3* (col/3) +i%3]==ch) return false;
         }
         return true;
     }
