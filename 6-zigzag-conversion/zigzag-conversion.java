639efaacb1a42d9d23e8e5320f3d546a1ae5f418
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2026-05-17 16:53:24 +0530

    Time: 8 ms (18.66%) | Memory: 47 MB (25.23%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,24 +1,22 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows==1 || s.length()<numRows){
-            return s;
-        }
-        List<List<Character>> arr=new ArrayList<>();
+        if (numRows==1) return s;
         StringBuilder sb=new StringBuilder();
+        List<List<Character>> arr=new ArrayList<>();
         for (int i=0;i<numRows;i++){
             arr.add(new ArrayList<>());
         }
         int i=0;
-        int direction=1;
+        int dir=1;
         for (char ch:s.toCharArray()){
             arr.get(i).add(ch);
             if (i==0){
-                direction=1;
+                dir=1;
             }
             else if (i==numRows-1){
-                direction=-1;
+                dir=-1;
             }
-            i=i+direction;
+            i+=dir;
         }
         for (List<Character> row:arr){
             for (char ch:row){
