a114156800da7753af3c7403a6024b3bd0edb8a4
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2026-05-20 21:47:46 +0530

    Time: 6 ms (24.13%) | Memory: 46.2 MB (6.91%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int reverse=0;
-        int temp=x;
-        while (x>0){
-            int rem=x%10;
-            reverse=reverse*10+rem;
-            x=x/10;
+        String str=x+"";
+        int i=0;
+        int j=str.length()-1;
+        while (i<=j){
+            if (str.charAt(i)!=str.charAt(j)) return false;
+            i++;
+            j--;
         }
-        return reverse==temp;
+        return true;
     }
 }
