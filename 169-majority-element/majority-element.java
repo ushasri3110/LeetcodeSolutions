730ef09eb453e07628bffcca3948242bbf28ddf5
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2026-05-28 16:57:01 +0530

    Time: 1 ms (99.87%) | Memory: 55.8 MB (30.95%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,12 +3,12 @@ class Solution {
         int majority=nums[0];
         int votes=1;
         for (int i=1;i<nums.length;i++){
-            if (votes==0){
+            if (nums[i]==majority){
+                votes++;
+            }
+            else if (votes==0){
                 majority=nums[i];
                 votes=1;
-            }
-            else if (nums[i]==majority){
-                votes++;
             }
             else{
                 votes--;
