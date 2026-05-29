cf3f4e0016fc053c367bba837cd668578213f8db
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2026-05-29 23:02:14 +0530

    Time: 59 ms (6.77%) | Memory: 92.4 MB (78.13%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,21 +1,47 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         HashSet<Integer> set=new HashSet<>();
+//         for (int i:nums){
+//             set.add(i);
+//         }
+//         int ans=0;
+//         for (int i:set){
+//             if (!set.contains(i-1)){
+//                 int count=1;
+//                 int current=i;
+//                 while (set.contains(current+1)){
+//                     current++;
+//                     count++;
+//                 }
+//                 ans=Math.max(ans,count);
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        Map<Integer,Boolean> map=new HashMap<>();
+        int longest=0;
         for (int i:nums){
-            set.add(i);
+            map.put(i,false);
         }
-        int ans=0;
-        for (int i:set){
-            if (!set.contains(i-1)){
-                int count=1;
-                int current=i;
-                while (set.contains(current+1)){
-                    current++;
-                    count++;
-                }
-                ans=Math.max(ans,count);
+        for (int i:nums){
+            int current=1;
+            int next=i+1;
+            while (map.containsKey(next) && map.get(next)==false){
+                current++;
+                map.put(next,true);
+                next++;
             }
+            int prev=i-1;
+            while (map.containsKey(prev) && map.get(prev)==false){
+                current++;
+                map.put(prev,true);
+                prev--;
+            }
+            longest=Math.max(longest,current);
         }
-        return ans;
+        return longest;
     }
 }
