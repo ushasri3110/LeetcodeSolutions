63077f3d53b0b018bc1b67e4e3084b7ed0e063e5
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2026-05-30 19:59:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (22.23%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans=new ArrayList<>();
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int rowStart=0,rowEnd=n-1,colStart=0,colEnd=m-1;
-        while (rowStart <= rowEnd && colStart <= colEnd){
-            for (int i=colStart;i<=colEnd;i++){
+        List<Integer> ans = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int rowStart = 0, rowEnd = n - 1, colStart = 0, colEnd = m - 1;
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            for (int i = colStart; i <= colEnd; i++) {
                 ans.add(matrix[rowStart][i]);
             }
             rowStart++;
-            for (int i=rowStart;i<=rowEnd;i++){
+            for (int i = rowStart; i <= rowEnd; i++) {
                 ans.add(matrix[i][colEnd]);
             }
             colEnd--;
 
-            if (rowStart<=rowEnd){
-                for (int i=colEnd;i>=colStart;i--){
-                ans.add(matrix[rowEnd][i]);
-            }
+            if (rowStart <= rowEnd) {
+                for (int i = colEnd; i >= colStart; i--) {
+                    ans.add(matrix[rowEnd][i]);
+                }
             }
             rowEnd--;
-            if (colStart<=colEnd){
-                for (int i=rowEnd;i>=rowStart;i--){
-                ans.add(matrix[i][colStart]);
+            if (colStart <= colEnd) {
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    ans.add(matrix[i][colStart]);
                 }
             }
             colStart++;
